db03d25ff6cb47e6b12a6c9af63d1aa92205ed51
DmitryeMaluckow <dim.maluckow2017@yandex.ru>
2026-03-31 14:48:40 +0300

    some improves

--- src/features/update-profile/use-update-profile.ts
+++ src/features/update-profile/use-update-profile.ts
@@ -1,54 +1,7 @@
+import { users } from '@/shared/consts';
 import { User } from '@/shared/types';
 
 export const useUpdateProfile = () => {
-  const users: User[] = [
-    {
-      id: 'u1',
-      name: 'Алексей Петров',
-      email: 'a.petrov@corp.com',
-      role: 'ADMIN',
-      notificationChannels: { email: true, telegram: true },
-    },
-    {
-      id: 'u2',
-      name: 'Мария Иванова',
-      email: 'm.ivanova@corp.com',
-      role: 'SUPPORT',
-      categoryIds: ['c1', 'c2', 'c6'],
-      notificationChannels: { email: true, telegram: false },
-    },
-    {
-      id: 'u3',
-      name: 'Дмитрий Соколов',
-      email: 'd.sokolov@corp.com',
-      role: 'SUPPORT',
-      categoryIds: ['c1', 'c3'],
-      notificationChannels: { email: true, telegram: true },
-    },
-    {
-      id: 'u4',
-      name: 'Елена Кузнецова',
-      email: 'e.kuznetsova@corp.com',
-      role: 'USER',
-      notificationChannels: { email: true, telegram: false },
-    },
-    {
-      id: 'u5',
-      name: 'Николай Волков',
-      email: 'n.volkov@corp.com',
-      role: 'USER',
-      notificationChannels: { email: false, telegram: true },
-    },
-    {
-      id: 'u6',
-      name: 'Ирина Смирнова',
-      email: 'i.smirnova@corp.com',
-      role: 'SUPPORT',
-      categoryIds: ['c3', 'c4', 'c5', 'c6'],
-      notificationChannels: { email: true, telegram: true },
-    },
-  ];
-
   const currentUser: User = users[0];
 
   return {
